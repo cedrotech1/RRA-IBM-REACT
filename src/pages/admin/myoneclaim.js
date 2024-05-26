4faa4bd8d69e9734d0094de54001c332b4afab06
cedrotech1 <cedrotech1@gmail.com>
2024-05-26 22:01:01 +0200

    updated

--- src/pages/admin/myoneclaim.js
+++ src/pages/admin/myoneclaim.js
@@ -9,138 +9,147 @@ import LoadingSpinner from '../../components/loading';
 import PdfViewer from '../../components/viewer';
 
 function App() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [claims, setClaims] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [pdfUrl1, setPdfUrl1] = useState('');
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [claims, setClaims] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pdfUrl1, setPdfUrl1] = useState('');
 
-    useEffect(() => {
-        const fetchClaims = async () => {
-            try {
-                const token = localStorage.getItem('token');
-                const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/one/${id}`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
+  useEffect(() => {
+    const fetchClaims = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/one/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-                const data = await response.json();
+        const data = await response.json();
 
-                if (data.success) {
-                    setClaims([data.data]);
-                    setPdfUrl1(data.data.file.replace('http://', 'https://'));
-                } else {
-                    console.error('Failed to fetch claims:', data.message);
-                }
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching claims:', error);
-                setLoading(false);
-            }
-        };
-
-        fetchClaims();
-    }, [id]);
-
-    const handleAction = async (action, userId) => {
-        const actions = {
-            approve: "approve",
-            reject: "reject",
-            check: "check",
-            uncheck: "uncheck"
-        };
-
-        const isConfirmed = window.confirm(`Are you sure you want to ${actions[action]} this claim?`);
-        if (!isConfirmed) return;
-
-        try {
-            const token = localStorage.getItem('token');
-            const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/${action}/${userId}`, {
-                method: 'PUT',
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-
-            if (response.ok) {
-                const res = await response.json();
-                toast.success(res.message);
-                await new Promise(resolve => setTimeout(resolve, 1000));
-                navigate('../employee_claim');
-            } else {
-                const errorData = await response.json();
-                console.error(`Failed to ${actions[action]} user with ID ${userId}:`, errorData.message);
-            }
-        } catch (error) {
-            console.error(`Error during ${actions[action]}:`, error);
+        if (data.success) {
+          setClaims([data.data]);
+          setPdfUrl1(data.data.file.replace('http://', 'https://'));
+        } else {
+          console.error('Failed to fetch claims:', data.message);
         }
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching claims:', error);
+        setLoading(false);
+      }
     };
 
-    return (
-        <>
-            <Menu />
-            <Hero title='Details of my claim' discription='details of this claim' />
-            <section className="section">
-                <div className="container">
-                    <div className="row justify-content-center text-center mb-5" data-aos="fade"></div>
+    fetchClaims();
+  }, [id]);
+
+  const handleAction = async (action, userId) => {
+    const actions = {
+      approve: "approve",
+      reject: "reject",
+      check: "check",
+      uncheck: "uncheck"
+    };
+
+    const isConfirmed = window.confirm(`Are you sure you want to ${actions[action]} this claim?`);
+    if (!isConfirmed) return;
+
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/${action}/${userId}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const res = await response.json();
+        toast.success(res.message);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        navigate('../employee_claim');
+      } else {
+        const errorData = await response.json();
+        console.error(`Failed to ${actions[action]} user with ID ${userId}:`, errorData.message);
+      }
+    } catch (error) {
+      console.error(`Error during ${actions[action]}:`, error);
+    }
+  };
+  console.log(claims)
+
+  return (
+    <>
+      <Menu />
+      <Hero title='Details of my claim' discription='details of this claim' />
+      <section className="section">
+        <div className="container">
+          <div className="row justify-content-center text-center mb-5" data-aos="fade"></div>
+          <div className="row">
+            {loading ? (
+              <LoadingSpinner />
+            ) : (
+              claims.map(claim => (
+                <div className="col-md-4" key={claim.id}>
+                  <div className="step">
+                    <span className="number">Claim number:#000{claim.id}</span>
+                    NAMES:{claim.ClaimsUser.firstname} &nbsp; {claim.ClaimsUser.lastname} <br />
+                    EMAIL:{claim.ClaimsUser.email}<br />
+                    PHONE:{claim.ClaimsUser.phone}<br />
+                    TIN NUMBER:{claim.ClaimsUser.tinnumber}<br />
+                    NATIONAL ID NUMBER:{claim.ClaimsUser.nid}<br />
+                    <h3>{claim.title}</h3>
+                    <p>{claim.description}</p>
                     <div className="row">
-                        {loading ? (
-                            <LoadingSpinner />
-                        ) : (
-                            claims.map(claim => (
-                                <div className="col-md-4" key={claim.id}>
-                                    <div className="step">
-                                        <span className="number">Claim number:#000{claim.id}</span>
-                                        <h3>{claim.title}</h3>
-                                        <p>{claim.description}</p>
-                                        <div className="row">
-                                            <div className="col-4"><button className="statusbtn">{claim.status}</button></div>
-                                            <div className="col-4"></div>
-                                            <div className="col-4"></div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-4"><i>{claim.date}</i></div>
-                                            <div className="col-4"></div>
-                                            <div className="col-4"><i>{claim.time}</i></div>
-                                        </div>
-                                        <div className="row">
-                                            {claim.status === 'pending' || claim.status === 'checked' ? (
-                                                <>
-                                                    <div className="col-4">
-                                                        <button onClick={() => handleAction('check', claim.id)} className="approvebtn">Check</button>
-                                                    </div>
-                                                    <div className="col-4">
-                                                        <button onClick={() => handleAction('uncheck', claim.id)} className="deletebtn">Uncheck</button>
-                                                    </div>
-                                                </>
-                                            ) : (
-                                                <>
-                                                    <div className="col-4">
-                                                        <button onClick={() => handleAction('approve', claim.id)} className="approvebtn">Approve</button>
-                                                    </div>
-                                                    <div className="col-4">
-                                                        <button onClick={() => handleAction('reject', claim.id)} className="deletebtn">Reject</button>
-                                                    </div>
-                                                </>
-                                            )}
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        )}
-                        {pdfUrl1 && (
-                            <div className="col-md-8">
-                                <PdfViewer pdfUrl={pdfUrl1} />
-                            </div>
-                        )}
+                      <div className="col-4"><button className="statusbtn">{claim.status}</button></div>
+                      <div className="col-4"></div>
+                      <div className="col-4"></div>
                     </div>
+                    <div className="row">
+                      <div className="col-12">
+                        time and date of claim <br />
+                        <i>{claim.date} &nbsp; &nbsp;  <i>{claim.time}</i></i>
+                      </div>
+
+                    </div>
+                    <br/>
+                    <div className="row">
+                      {claim.status === 'pending' || claim.status === 'checked' ? (
+                        <>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('check', claim.id)} className="approvebtn">Check</button>
+                          </div>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('uncheck', claim.id)} className="deletebtn">Uncheck</button>
+                          </div>
+                        </>
+                      ) : (
+                        <>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('approve', claim.id)} className="approvebtn">Approve</button>
+                          </div>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('reject', claim.id)} className="deletebtn">Reject</button>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  </div>
                 </div>
-            </section>
-            <Footer />
-            <ToastContainer />
-        </>
-    );
+              ))
+            )}
+            {pdfUrl1 && (
+              <div className="col-md-8">
+                <PdfViewer pdfUrl={pdfUrl1} />
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+      <Footer />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
--- src/pages/employee/myoneclaim.js
+++ src/pages/employee/myoneclaim.js
@@ -1,28 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Menu from '../../components/employee';
 import Footer from '../../components/footer';
 import Hero from '../../components/samplehero';
-
-import React, { useEffect, useState } from 'react';
 import LoadingSpinner from '../../components/loading';
-import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import PdfViewer from '../../components/viewer';
-
 
 function App() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [claims, setclaims] = useState();
+  const [claims, setClaims] = useState([]);
   const [loading, setLoading] = useState(true);
-  const token = localStorage.getItem('token');
-  const [pdfUrl1, setpdfUrl1] = useState('');
-
-
+  const [pdfUrl1, setPdfUrl1] = useState('');
 
   useEffect(() => {
-    const fetchclaims = async () => {
+    const fetchClaims = async () => {
       try {
         const token = localStorage.getItem('token');
         const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/one/${id}`, {
@@ -34,41 +28,35 @@ function App() {
         const data = await response.json();
 
         if (data.success) {
-          // if (!Array.isArray(data.data)) {
-          //   data = [data.data];
-          // }
-          setclaims([data.data]);
-          setpdfUrl1(data.data.file)
-          console.log(data.data.file); // Log URL to check
-       
+          setClaims([data.data]);
+          setPdfUrl1(data.data.file.replace('http://', 'https://'));
         } else {
           console.error('Failed to fetch claims:', data.message);
         }
-
-        // Set loading to false after fetching data
         setLoading(false);
       } catch (error) {
         console.error('Error fetching claims:', error);
-        // Set loading to false in case of an error
         setLoading(false);
       }
     };
 
-    fetchclaims();
-  }, []);
+    fetchClaims();
+  }, [id]);
 
-  // 
-  const handleunCheck = async (userId) => {
-    // navigate(`../one/${id}`);
+  const handleAction = async (action, userId) => {
+    const actions = {
+      approve: "approve",
+      reject: "reject",
+      check: "check",
+      uncheck: "uncheck"
+    };
+
+    const isConfirmed = window.confirm(`Are you sure you want to ${actions[action]} this claim?`);
+    if (!isConfirmed) return;
 
     try {
-      const isConfirmed = window.confirm("Are you sure you want to check this claim?");
-      if (!isConfirmed) {
-        return;
-      }
-
       const token = localStorage.getItem('token');
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/uncheck/${userId}`, {
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/${action}/${userId}`, {
         method: 'PUT',
         headers: {
           Authorization: `Bearer ${token}`,
@@ -78,252 +66,86 @@ function App() {
       if (response.ok) {
         const res = await response.json();
         toast.success(res.message);
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the delay time as needed
-
-        // window.location.reload();
-        await navigate('../employee_claim');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        navigate('../employee_claim');
       } else {
         const errorData = await response.json();
-        // toast.error(res.message);
-        // console.error(`Failed to approve user with ID ${userId}:`, errorData.message);
+        console.error(`Failed to ${actions[action]} user with ID ${userId}:`, errorData.message);
       }
     } catch (error) {
-      console.error('Error deleting user:', error);
+      console.error(`Error during ${actions[action]}:`, error);
     }
   };
-  const handleCheck = async (userId) => {
-  
+  console.log(claims)
 
-    try {
-      const isConfirmed = window.confirm("Are you sure you want to check this claim?");
-      if (!isConfirmed) {
-        return;
-      }
-
-      const token = localStorage.getItem('token');
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/check/${userId}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (response.ok) {
-        const res = await response.json();
-        toast.success(res.message);
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the delay time as needed
-
-        // window.location.reload();
-        await navigate('../employee_claim');
-      } else {
-        const errorData = await response.json();
-        // toast.error(res.message);
-        // console.error(`Failed to approve user with ID ${userId}:`, errorData.message);
-      }
-    } catch (error) {
-      console.error('Error deleting user:', error);
-    }
-  };
-
-  const handleApprove = async (userId) => {
-    // navigate(`../one/${id}`);
-
-    try {
-      const isConfirmed = window.confirm("Are you sure you want to approve this claim?");
-      if (!isConfirmed) {
-        return;
-      }
-
-      const token = localStorage.getItem('token');
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/approve/${userId}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (response.ok) {
-        const res = await response.json();
-        toast.success(res.message);
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the delay time as needed
-
-        // window.location.reload();
-        await navigate('../employee_claim');
-      } else {
-        const errorData = await response.json();
-        console.error(`Failed to approve user with ID ${userId}:`, errorData.message);
-      }
-    } catch (error) {
-      console.error('Error deleting user:', error);
-    }
-  };
-
-  const handleReject = async (userId) => {
-    // navigate(`../one/${id}`);
-
-
-    try {
-      const isConfirmed = window.confirm("Are you sure you want to reject this claim?");
-      if (!isConfirmed) {
-        return;
-      }
-
-      const token = localStorage.getItem('token');
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/v1/Claim/reject/${userId}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (response.ok) {
-        const res = await response.json();
-        toast.success(res.message);
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the delay time as needed
-
-        // window.location.reload();
-        await navigate('../employee_claim');
-      } else {
-        const errorData = await response.json();
-        console.error(`Failed to approve user with ID ${userId}:`, errorData.message);
-      }
-    } catch (error) {
-      console.error('Error deleting user:', error);
-    }
-  };
-  // console.log(claims)
-
-  console.log(pdfUrl1);
   return (
     <>
       <Menu />
       <Hero title='Details of my claim' discription='details of this claim' />
-
-
-
-
-
-      <section class="section">
-
-        <div class="container">
-          <div class="row justify-content-center text-center mb-5" data-aos="fade">
-          </div>
-
-          <div class="row">
+      <section className="section">
+        <div className="container">
+          <div className="row justify-content-center text-center mb-5" data-aos="fade"></div>
+          <div className="row">
             {loading ? (
               <LoadingSpinner />
             ) : (
-              claims.map((claim) => (
-
-
-                <div class="col-md-4" key={claim.id}>
-                  <div class="step">
-                    <span class="number">Claim number:#000{claim.id}</span>
+              claims.map(claim => (
+                <div className="col-md-4" key={claim.id}>
+                  <div className="step">
+                    <span className="number">Claim number:#000{claim.id}</span>
+                    NAMES:{claim.ClaimsUser.firstname} &nbsp; {claim.ClaimsUser.lastname} <br />
+                    EMAIL:{claim.ClaimsUser.email}<br />
+                    PHONE:{claim.ClaimsUser.phone}<br />
+                    TIN NUMBER:{claim.ClaimsUser.tinnumber}<br />
+                    NATIONAL ID NUMBER:{claim.ClaimsUser.nid}<br />
                     <h3>{claim.title}</h3>
                     <p>{claim.description}</p>
-                    <br />
-                    <div className='row'>
-                      <div className='col-4'>  <i><button className='statusbtn'>{claim.status}</button> </i></div>
-                      <div className='col-4'> </div>
-                      <div className='col-4'> <i></i> </div>
-
+                    <div className="row">
+                      <div className="col-4"><button className="statusbtn">{claim.status}</button></div>
+                      <div className="col-4"></div>
+                      <div className="col-4"></div>
+                    </div>
+                    <div className="row">
+                      <div className="col-12">
+                        time and date of claim <br />
+                        <i>{claim.date} &nbsp; &nbsp;  <i>{claim.time}</i></i>
+                      </div>
 
                     </div>
-                    <div className='row'>
-                      <div className='col-4'>  <i>{claim.date} </i></div>
-                      <div className='col-4'> </div>
-                      <div className='col-4'> <i>{claim.time} </i> </div>
-                    </div>
-
-                    <div className='row'>
-                      <br />
-                      {(() => {
-  if (claim.status === 'pending' || claim.status === 'checked' ) {
-    return (
-      <>
-      <div className='col-4'> 
-      <br /><button onClick={() => handleCheck(claim.id)} className='approvebtn'>
-            Check
-          </button>
-        </div>
-
-           <div className='col-4'> 
-           <br /> <button onClick={() => handleunCheck(claim.id)} className='deletebtn'>
-            unheck
-          </button>
-        </div>
-        </>
-    );
-  }
-})()}
-                     
-                    
-
-
-                      {(() => {
-  if (claim.status === 'rib-approved'  || claim.status === 'approved'  || claim.status === 'rejected') {
-    return (
-      <>
-      <div className='col-4'> 
-      <br /><button onClick={() => handleApprove(claim.id)} className='approvebtn'>approve</button>
-        </div>
-
-           <div className='col-4'> 
-           <br /> <button onClick={() => handleReject(claim.id)} className='deletebtn'>reject</button>
-        </div>
-        </>
-    );
-  }
-})()}
-
-
-
-
-
-
-
-
-
-
-
-
-                    
+                    <br/>
+                    <div className="row">
+                      {claim.status === 'pending' || claim.status === 'checked' ? (
+                        <>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('check', claim.id)} className="approvebtn">Check</button>
+                          </div>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('uncheck', claim.id)} className="deletebtn">Uncheck</button>
+                          </div>
+                        </>
+                      ) : (
+                        <>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('approve', claim.id)} className="approvebtn">Approve</button>
+                          </div>
+                          <div className="col-4">
+                            <button onClick={() => handleAction('reject', claim.id)} className="deletebtn">Reject</button>
+                          </div>
+                        </>
+                      )}
                     </div>
                   </div>
                 </div>
               ))
             )}
-             
-
-             {loading ? (
-  <LoadingSpinner />
-) : (
-  <>
-    {claims.map((claim) => (
-      <>
-      </>
-
-
-      // Your existing code for rendering claim details
-    ))}
-    
-    {pdfUrl1 && (
-      <div class="col-md-8">
-        <PdfViewer pdfUrl={pdfUrl1} />
-      </div>
-    )}
-  </>
-)}
-
-            
-
+            {pdfUrl1 && (
+              <div className="col-md-8">
+                <PdfViewer pdfUrl={pdfUrl1} />
+              </div>
+            )}
           </div>
         </div>
-
       </section>
-
-
-
       <Footer />
       <ToastContainer />
     </>
